15ff189c2056d14cc500ae45c10247d26b28c963
Vesylivskyy Bogdan <vesylivskyy@gmail.com>
2019-11-28 17:09:53 +0200

    Add Routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  template: `<div>
+    <h1>Angular routes</h1>
+    <nav>
+      <a routerLink="">Home</a>
+      <a routerLink="/profile">Profile</a>
+      <a routerLink="contact">Contact</a>
+    </nav>
+    <router-outlet></router-outlet>
+  </div>`
 })
 export class AppComponent {
   title = 'profile-angular-project';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
-import { ProfilesComponent } from './components/profiles/profiles.component';
 
 import { MatTabsModule } from '@angular/material/tabs';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
+
+
+import {MatTableModule} from '@angular/material/table';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
+
+
+import {Routes, RouterModule} from '@angular/router';
+import { AppComponent } from './app.component';
+import { ProfilesComponent } from './components/profiles/profiles.component';
 import { ContactsComponent } from './components/contacts/contacts.component';
+
+ 
+// определение маршрутов
+const appRoutes: Routes =[
+    { path: '', component: AppComponent},
+    { path: 'profile', component: ProfilesComponent},
+    { path: 'contact', component: ContactsComponent}
+];
+
 
 
 @NgModule({
@@ -23,7 +40,10 @@ import { ContactsComponent } from './components/contacts/contacts.component';
     AppRoutingModule,
     MatTabsModule,
     BrowserAnimationsModule,
-    MatCardModule
+    MatCardModule,
+    MatTableModule,
+    MatCheckboxModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]

1077bf93c8940a1ef76903efba4e4bed8b575780
Vesylivskyy Bogdan <vesylivskyy@gmail.com>
2019-11-28 15:08:54 +0200

    Сreated profile page layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-
+import { ProfilesComponent } from './components/profiles/profiles.component';
 
 import { MatTabsModule } from '@angular/material/tabs';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ProfilesComponent } from './components/profiles/profiles.component';
+import { MatCardModule } from '@angular/material/card';
+import { ContactsComponent } from './components/contacts/contacts.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfilesComponent
+    ProfilesComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatTabsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/profiles/profiles.component.ts
+++ src/app/components/profiles/profiles.component.ts
@@ -9,9 +9,9 @@ import { Component, OnInit } from '@angular/core';
 export class ProfilesComponent implements OnInit {
 
   photo = 'photo';
-  name: string;
-  surname: string;
-  birth: number;
+  name = 'name';
+  surname = 'surname';
+  birth = 'birth';
   number = 'phone number';
 
   constructor() { }
